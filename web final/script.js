f369add45ec2fb8dfd40e8753a64de01a61ec312
green1179 <60520661+green1179@users.noreply.github.com>
2020-02-03 15:56:50 -0700

    added slideshow
    still getting used to this so sorry if I messsed anything up lmk if I did and ill fix it asap

--- web final/script.js
+++ web final/script.js
@@ -1,1 +1,19 @@
-//comment
+var image = document.getElementById("image");
+ 
+var imageArray = ["images/elon1.png","images/Elon2.png","images/elon3.png"];
+ 
+var imageIndex = 0;
+ 
+function changeImage() {
+   image.setAttribute("src",imageArray[imageIndex]);
+   imageIndex++;
+   if (imageIndex >= imageArray.length) {
+      imageIndex = 0;
+   }
+}
+ 
+window.onload = function() {
+ 
+   setInterval(changeImage,2000);
+ 
+}
