8f8bd63c68a7233457975e683e14e5373afc5c42
snajera531 <60483863+snajera531@users.noreply.github.com>
2020-02-03 14:16:30 -0700

    quote json?
    just want to see what i did wrong lol

--- web final/script.js
+++ web final/script.js
@@ -1,1 +1,9 @@
-//comment
+const dailyquote = document.getElementById('dailyquote');
+
+let url = `https://theysaidso.com/quote/paul-brown-when-you-win-say-nothing-when-you-lose-say-less`;
+
+fetch(url)
+    .then(response.json())
+    .then(data => {
+        dailyquote.innerHTML = contents[0].quotes[0].quote;
+    })
