218cb4816ce99f9eec9998f529e1bd8dffda74cb
snajera531 <60483863+snajera531@users.noreply.github.com>
2020-02-04 11:12:06 -0700

    update

--- web final/script.js
+++ web final/script.js
@@ -5,7 +5,8 @@ let url = `https://theysaidso.com/quote/paul-brown-when-you-win-say-nothing-when
 fetch(url)
     .then(response => response.json())
     .then(data => {
-        dailyquote.innerHTML = contents[0].quotes[0].quote;
+        console.log(data);
+        dailyquote.innerHTML = data.list[0].contents[0].quotes[0].quote;
     })
 
 
