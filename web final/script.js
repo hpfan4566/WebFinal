a677db9510ec45216280da1868c622a303305a41
snajera531 <60483863+snajera531@users.noreply.github.com>
2020-02-03 17:00:57 -0700

    quote adjustment

--- web final/script.js
+++ web final/script.js
@@ -3,7 +3,7 @@ const dailyquote = document.getElementById('dailyquote');
 let url = `https://theysaidso.com/quote/paul-brown-when-you-win-say-nothing-when-you-lose-say-less`;
 
 fetch(url)
-    .then(response.json())
+    .then(response => response.json())
     .then(data => {
         dailyquote.innerHTML = contents[0].quotes[0].quote;
     })
