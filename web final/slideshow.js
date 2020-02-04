4ace519d878f407f4b73f44d9a4a57c59cc4682c
green1179 <60520661+green1179@users.noreply.github.com>
2020-02-04 11:57:26 -0700

    moar images

--- web final/slideshow.js
+++ web final/slideshow.js
@@ -1,6 +1,6 @@
 var image = document.getElementById("image");
  
-var imageArray = ["images/elonQuote1.jpg","images/elon1.png","images/elonQuote2.jpg","images/Elon2.png","elonQuote3.jpg","images/elon3.png"];
+var imageArray = ["images/space1.png","images/elonQuote1.jpg","images/space2.png","images/elon1.png","images/elonQuote2.jpg","images/Elon2.png","images/space3.png","elonQuote3.jpg","images/elon3.png","images/space4.png"];
  
 var imageIndex = 0;
  
